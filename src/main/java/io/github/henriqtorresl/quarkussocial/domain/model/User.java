275e2b3d2dcdcc78046d084d0b855e8def017bb3
henriqtorresl <henriquetlandin@gmail.com>
2024-02-15 11:09:55 -0300

    adicionando o lombok ao projeto

--- src/main/java/io/github/henriqtorresl/quarkussocial/domain/model/User.java
+++ src/main/java/io/github/henriqtorresl/quarkussocial/domain/model/User.java
@@ -3,11 +3,13 @@ package io.github.henriqtorresl.quarkussocial.domain.model;
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.*;
+import lombok.Data;
 
 import java.util.Objects;
 
 @Entity
 @Table(name = "USERS")      // se eu não colocasse essa anotação, o Java entenderia que o nome da tabela é User (que é o nome da classe)
+@Data   // lombok
 public class User {
 
     @Id
@@ -18,40 +20,4 @@ public class User {
     @Column(name = "age")
     private Integer age;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        User user = (User) o;
-        return Objects.equals(id, user.id) && Objects.equals(name, user.name) && Objects.equals(age, user.age);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, age);
-    }
 }
--- src/main/java/io/github/henriqtorresl/quarkussocial/rest/dto/CreateUserRequest.java
+++ src/main/java/io/github/henriqtorresl/quarkussocial/rest/dto/CreateUserRequest.java
@@ -2,7 +2,10 @@ package io.github.henriqtorresl.quarkussocial.rest.dto;
 
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 
+
+@Data       // essa annotation é um conglomerado de varias annotations do lombok (@Getter, @Setter ...)
 public class CreateUserRequest {
 
     // Após adicionar essas annotations eu consigo usar o validator pra validar esses campos...
@@ -10,20 +13,4 @@ public class CreateUserRequest {
     private String name;
     @NotNull(message = "Age is Required")
     private Integer age;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public Integer getAge() {
-        return age;
-    }
-
-    public void setAge(Integer age) {
-        this.age = age;
-    }
 }
